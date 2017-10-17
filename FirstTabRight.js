683eda3b290585553d8ec28a9c1c29bcddb5e8e4
charspan <1902168804@qq.com>
2017-10-17 15:47:23 +0800

    1. 工程列表界面改用 flatlist 尝试提升安卓运行速度

--- FirstTabRight.js
+++ FirstTabRight.js
@@ -9,7 +9,8 @@ import {
   TouchableOpacity,
   Alert,
   Modal,
-  AsyncStorage
+  AsyncStorage,
+  FlatList
 } from'react-native';
 import {base_url,httpPostJson} from './common';
 import TextInputBar from './my_component/TextInputBar';
@@ -20,6 +21,11 @@ export default class FirstTabRight extends Component{
   
   constructor(props){    
     super(props);
+
+    // for(var i=0;i<props.projects.length;i++){
+    //   props.projects[i].key=i;
+    // }
+
     this.state={
       // 接收 http 请求头
       header: props.header,
@@ -37,70 +43,141 @@ export default class FirstTabRight extends Component{
       callbackShowRooms: props.callbackShowRooms
     }
   }
+
+
+
   
-  // 绑定上下文 方法二 ()=>{}
-  renderRow=(rowData,sectionID, rowID)=> {
-      var img = this.state.rights[rowData.id]==undefined ? require('./img/unChecked.png') : require('./img/checked.png');
-      return (
-        <View>
+  // // 绑定上下文 方法二 ()=>{}
+  // renderRow=(rowData,sectionID, rowID)=> {
+  //     var img = this.state.rights[rowData.id]==undefined ? require('./img/unChecked.png') : require('./img/checked.png');
+  //     return (
+  //       <View>
         
-          <View style={styles.row}>
-            <TouchableOpacity // 点击勾选当前工程
-              style={{flex:1}}
-              onPress={()=>{
-                if(this.state.rights[rowData.id]==undefined){
-                  this.state.rights[rowData.id]='.';
-                }else{
-                  delete this.state.rights[rowData.id];
-                }
-                this.setState({
-                  rights: this.state.rights
-                });
-                console.log("子页面right:",this.state.rights);
-              }}
-            >
-              <Image
-                style={styles.thumb}
-                source={img}
-              />
-            </TouchableOpacity>
-            <TouchableOpacity // 点击查看工程详情
-              style={{flex:3}}
-              onPress={()=>{
-                //console.log("第"+rowID+"行被点击了");
-                Alert.alert('工程详情',JSON.stringify(rowData),[{text: '确定'}]);
-              }}
-            >
-              <Text style={{fontSize:16,color:'blue'}}>
-                {"编号: "+rowData.id+"  名称: "+rowData.name + "\n备注: " +rowData.remark}
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity //点击修改子账号权限信息
-              onPress={()=>{
-                // 隐藏工程列表显示房间列表
-                this.state.callbackShowRooms(rowData.id);//,this.state.rights);
-              }}
-            >
-              <Image style={styles.thumb} source={require('./img/power.png')} />
-            </TouchableOpacity>
-          </View>
-          <View style={{height:2,backgroundColor:'white'}} />
-        </View>
-      );
-  }
+  //         <View style={styles.row}>
+  //           <TouchableOpacity // 点击勾选当前工程
+  //             style={{flex:1}}
+  //             onPress={()=>{
+  //               if(this.state.rights[rowData.id]==undefined){
+  //                 this.state.rights[rowData.id]='.';
+  //               }else{
+  //                 delete this.state.rights[rowData.id];
+  //               }
+  //               this.setState({
+  //                 rights: this.state.rights
+  //               });
+  //               console.log("子页面right:",this.state.rights);
+  //             }}
+  //           >
+  //             <Image
+  //               style={styles.thumb}
+  //               source={img}
+  //             />
+  //           </TouchableOpacity>
+  //           <TouchableOpacity // 点击查看工程详情
+  //             style={{flex:3}}
+  //             onPress={()=>{
+  //               //console.log("第"+rowID+"行被点击了");
+  //               Alert.alert('工程详情',JSON.stringify(rowData),[{text: '确定'}]);
+  //             }}
+  //           >
+  //             <Text style={{fontSize:16,color:'blue'}}>
+  //               {"编号: "+rowData.id+"  名称: "+rowData.name + "\n备注: " +rowData.remark}
+  //             </Text>
+  //           </TouchableOpacity>
+  //           <TouchableOpacity //点击修改子账号权限信息
+  //             onPress={()=>{
+  //               // 隐藏工程列表显示房间列表
+  //               this.state.callbackShowRooms(rowData.id);//,this.state.rights);
+  //             }}
+  //           >
+  //             <Image style={styles.thumb} source={require('./img/power.png')} />
+  //           </TouchableOpacity>
+  //         </View>
+  //         <View style={{height:2,backgroundColor:'white'}} />
+  //       </View>
+  //     );
+  // }
   
+
+//列表的每一行
+renderItem=({item,index})=> {
+  
+  console.log("加载了第"+index+"行");
+  var img = this.state.rights[item.id]==undefined ? require('./img/unChecked.png') : require('./img/checked.png');
+  return (
+    <View>  
+      <View style={styles.row}>
+        <TouchableOpacity // 点击勾选当前工程
+          style={{flex:1}}
+          onPress={()=>{
+            if(this.state.rights[item.id]==undefined){
+              this.state.rights[item.id]='.';
+            }else{
+              delete this.state.rights[item.id];
+            }
+            this.setState({
+              rights: this.state.rights
+            });
+            console.log("工程权限情况right:",this.state.rights);
+          }}
+        >
+          <Image
+            style={styles.thumb}
+            source={img}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity // 点击查看工程详情
+          style={{flex:3}}
+          onPress={()=>{
+            //console.log("第"+rowID+"行被点击了");
+            Alert.alert('工程详情',JSON.stringify(item),[{text: '确定'}]);
+          }}
+        >
+          <Text style={{fontSize:16,color:'blue'}}>
+            {"编号: "+item.id+"  名称: "+item.name + "\n备注: " +item.remark}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity //点击修改子账号权限信息
+          onPress={()=>{
+            // 隐藏工程列表显示房间列表
+            this.state.callbackShowRooms(item.id);//,this.state.rights);
+          }}
+        >
+          <Image style={styles.thumb} source={require('./img/power.png')} />
+        </TouchableOpacity>
+      </View>
+      <View style={{height:2,backgroundColor:'white'}} />
+    </View>
+  );
+}
+
   render() {
     return (
-      <ListView
-        dataSource={this.state.dataSource.cloneWithRows(this.state.projects)}
-        renderRow={this.renderRow}
-        isSubAccountEditShowsVerticalScrollIndicator={false}
-        enableEmptySections = {true}
-        initialListSize={4}
-        pageSize={1}
-        onEndReached={()=>{
-          console.log("projects onEndReached");
-        }}
+      <FlatList
+        //dataSource={this.state.dataSource.cloneWithRows(this.state.projects)}
+        data={this.state.projects}
+        // renderRow={this.renderRow}
+        // isSubAccountEditShowsVerticalScrollIndicator={false}
+        // enableEmptySections = {true}
+        // initialListSize={4}
+        // pageSize={1}
+        // onEndReached={()=>{
+        //   console.log("projects onEndReached");
+        // }}
+       // renderItem={({item,index}) => {<Text>index</Text>}}
+       initialNumToRender={4}
+       refreshing={true}
+       keyExtractor={(item, index) => item.id}
+       getItemLayout={(data, index) => ( {length: 20, offset: 20 * index, index} )}
+       extraData={this.state}
+       numColumns={1}
+       renderItem={this.renderItem}
+      //  onViewableItemsChanged={
+      //    (info, changed) => {
+      //       console.log(info);
+      //       console.log(changed);
+      //     }
+      //   }
       />
     );
   }
